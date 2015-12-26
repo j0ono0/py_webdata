5ef5daa8c69c140bab0fd757937782d60f7ccd68
j0ono0 <john@johnnewall.com>
2015-12-26 11:20:42 +1030

    Retrieve data from the internet
    Some experiments and practice in retrieving data from the internet using
    Python.
    
    This version gets current Adelaide weather from BOM

---
+++ webdata.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import urllib.request
+
+response = urllib.request.urlopen('ftp://ftp2.bom.gov.au/anon/gen/fwo/IDA00100.dat')
+data = response.read().decode(encoding="UTF-8")
+lines = data.split("\n")
+for count, line in enumerate(lines):
+	lines[count] = line.split("#")
+	if lines[count][0] == "Adelaide":
+		adlref = count
+
+print ("current weather in %s: %s and %s" % (lines[adlref][0], lines[adlref][6], lines[adlref][7].lower()))
