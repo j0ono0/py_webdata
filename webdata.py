cdeb9fc5c86aaa2f8a6659d1cf8edf93d99be87f
j0ono0 <john@johnnewall.com>
2016-01-12 08:38:07 +1030

    focussed app onto web page analysis
    * removed unrelated functions
    * split processes into smaller functions

--- webdata.py
+++ webdata.py
@@ -1,74 +1,57 @@
 #!/usr/bin/env python
 
 import urllib.request
-import xml.etree.ElementTree as ET
-import time
 from bs4 import BeautifulSoup
 import re
 
-def weather_adelaide():
-	response = urllib.request.urlopen('ftp://ftp2.bom.gov.au/anon/gen/fwo/IDA00100.dat')
-	data = response.read().decode(encoding="UTF-8")
-	lines = data.split("\n")
-	for count, line in enumerate(lines):
-		lines[count] = line.split("#")
-		if lines[count][0] == "Adelaide":
-			adlref = count
-	forcasttime = time.strptime(lines[adlref][2],"%Y%m%d")
-	forcasttimestr = time.strftime("%d/%m/%y",forcasttime)
-	print ("Forecast weather in %s on %s: %s and %s" % (lines[adlref][0],forcasttimestr, lines[adlref][6], lines[adlref][7].lower()))
+def getKey(item):
+	return item[1]
 
-def news_just_in():
-	response = urllib.request.urlopen('http://www.abc.net.au/news/feed/51120/rss.xml')
-	tree = ET.parse(response)
-	root = tree.getroot()[0]
-	titles = []
-	for item in root.iter('item'):
-		titles.append(item.find('title').text)
+def validated_url(url):
+	if url[0:7] != 'http://' and url[0:8] != 'https://':
+		url = 'http://' + url
+	return url
 
-	print('Last 10 news updates from ABC news website:')		
-	for i in range(10):
-		print('%d: %s' %(i+1,titles[i]))
-
-def scrapepage(url):
-	def getKey(item):
-		return item[1]
-	def validateurl(url):
-		if url[0:7] != 'http://' and url[0:8] != 'https://':
-			url = 'http://' + url
-		return url	
-	
-	url = validateurl(url)
-	print("retrieving page from %s..." % url)
+def loadpage(url):
+	url = validated_url(url)
 	response = urllib.request.urlopen(url)
-	data = response.read()
-	soup = BeautifulSoup(data,'html.parser')
+	return response.read()
+
+def list_words(page):
+	soup = BeautifulSoup(page,'html.parser')
 	[tag.extract() for tag in soup(['head','script'])]
 	text = re.sub('\W', ' ', soup.get_text(' '))
 	text = re.sub(" {1,}", " ", text)
-	lst = text.split(" ")
-	words = {}
-	for word in lst:
-		word = word.lower()
-		if word in words.keys():
-			words[word] += 1
-		else:
-			words[word] = 1
-	print("unique word count: ", len(words))
-	wordlst = words.items()
-	wordlst = sorted(wordlst, key = getKey, reverse = True)
+	return text.split(" ")	
 	
-	file = open('page-analysis.txt', 'w')
-	file.write("%s different word on the page\n" % len(wordlst))
+def unique_words(page):
+	wordlst = list_words(page)
+	worddict = {}
+	for word in wordlst:
+		word = word.lower()
+		if word in worddict.keys():
+			worddict[word] += 1
+		else:
+			worddict[word] = 1
+	print("unique word count: ", len(worddict))
+	return worddict
+
+def dict_to_file(data,file_name):
+	#convert dict to lsit of tuples and sort
+	wordlst = data.items()
+	wordlst = sorted(wordlst, key = getKey, reverse = True)
+	#output to file
+	file = open(file_name, 'w')
+	file.write("%s Number of entries: \n" % len(wordlst))
 	for entry in wordlst:
 		file.write("%s: %s\n" %(entry[1],entry[0]))
 	file.close()
-		
-print("------------------")
-# news_just_in()
-print("------------------")
-# weather_adelaide()
-print("------------------")
+	print("data written to: %s" % file_name)
+
 url = input("enter URL to analyse: ")
-scrapepage(url)
+#scrapepage(url)
+page = loadpage(url)
+unique_words = unique_words(page)
+dict_to_file(unique_words,'unique-words.txt')
+
 input("press enter to end script.")
